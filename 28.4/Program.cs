3ba73121e7141fceae1dc7b14040f8b6da629e91
Atamanyuk2008 <atamaniuk.aleksandr@gmail.com>
2025-03-26 00:27:47 +0300

    Add files via upload

---
+++ 28.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] mas = { 63.5, 63.2, 63.57, 62.87, 64.31, 63.5, 63.24, 64.12, 62.59, 63.78, 63.42, 63.77 };
+            Idk(mas);
+            Console.ReadKey();
+        }
+        static void Idk(double[] mas)
+        {
+            double average = mas.Average();
+
+            double max = mas.Max();
+            double min = mas.Min();
+
+            int maxDay = Array.IndexOf(mas, max) + 1;
+            int minDay = Array.IndexOf(mas, min) + 1;
+
+            Console.WriteLine($"Среднее значение ЗП = {average:F2}");
+            Console.WriteLine($"Максимальная зп в {maxDay} день равна {max:F2}");
+            Console.WriteLine($"Минимальная зп в {minDay} день равна {min:F2}");
+        }
+    }
+}
